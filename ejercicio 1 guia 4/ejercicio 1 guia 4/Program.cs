fd24404eea117955d3843ce4a1e4c647ab7ba923
MorenaJv28 <124007217+MorenaJv28@users.noreply.github.com>
2023-09-28 22:26:25 -0600

    EJ1

---
+++ ejercicio 1 guia 4/ejercicio 1 guia 4/Program.cs
@@ -0,0 +1,22 @@
+﻿//Ejercicio 1
+using System.Runtime.ConstrainedExecution;
+
+Console.WriteLine(@"Realice un programa que permita ingresar una cadena de caracteres y se evalúe su
+tamaño, si el tamaño es menor que 25 caracteres que rellene con el carácter signo
+de dólar($), puede ser relleno por la derecha o por la izquierda.");
+
+Console.WriteLine("Ingrese una cadena de caracteres: ");
+string C = Console.ReadLine();
+
+
+int LDeseada = 25;
+int LActual = C.Length;
+
+
+C = C.PadLeft(LDeseada, '$');
+
+
+C = C.Substring(0, LDeseada);
+
+
+Console.WriteLine($"Cadena resultante: {C}");
